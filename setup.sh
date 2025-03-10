e23d8ece227d7afecfbcffd3d39d35e7c58ce87f
Ran Zeller <ran.zeller@solaredge.com>
2025-03-10 16:01:09 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -4,8 +4,32 @@
 sudo yum update && sudo yum upgrade -y
 
 # Install Node.js and npm
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
 sudo yum install -y nodejs
+
+# Install Chrome dependencies
+sudo yum install -y \
+    alsa-lib.x86_64 \
+    atk.x86_64 \
+    cups-libs.x86_64 \
+    gtk3.x86_64 \
+    ipa-gothic-fonts \
+    libXcomposite.x86_64 \
+    libXcursor.x86_64 \
+    libXdamage.x86_64 \
+    libXext.x86_64 \
+    libXi.x86_64 \
+    libXrandr.x86_64 \
+    libXScrnSaver.x86_64 \
+    libXtst.x86_64 \
+    pango.x86_64 \
+    xorg-x11-fonts-100dpi \
+    xorg-x11-fonts-75dpi \
+    xorg-x11-fonts-cyrillic \
+    xorg-x11-fonts-misc \
+    xorg-x11-fonts-Type1 \
+    xorg-x11-utils \
+    nss.x86_64
 
 # Install FFmpeg
 sudo yum install -y ffmpeg
@@ -13,7 +37,7 @@ sudo yum install -y ffmpeg
 # Install PM2 globally
 sudo npm install -p pm2@latest -g
 
-# Create logs directory
+# Create required directories
 mkdir -p logs
 mkdir -p .wwebjs_auth
 
@@ -49,4 +73,4 @@ EOL
 npm install
 
 # Start the application
-npm run prod 
+npm run prod
