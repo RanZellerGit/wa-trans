dfce86edf6edbbb3ff95f3992c41cef38e269104
Ran Zeller <ran.zeller@solaredge.com>
2025-03-16 14:45:49 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,5 @@ pm2 delete whatsapp-bot
 pm2 flush
 
 cd /home/ec2-user/wa-trans
-export OPENAI_API_KEY=$(aws ssm get-parameter --name /wa-bot/openAiApi --with-decryption --query "Parameter.Value" --output text)
-export DB_USER=$(aws ssm get-parameter --name /wa-bot/db_user --with-decryption --query "Parameter.Value" --output text) 
-export DB_PASSWORD=$(aws ssm get-parameter --name /wa-bot/db_password --with-decryption --query "Parameter.Value" --output text) 
-export DB_NAME=$(aws ssm get-parameter --name /wa-bot/db_name --with-decryption --query "Parameter.Value" --output text) 
-export NODE_ENV="production" 
-export PORT="3000" 
 
 pm2 start ecosystem.config.js 
